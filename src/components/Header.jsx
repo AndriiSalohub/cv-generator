58e44e912084f7c9c1e99ef763a1a7cebe156c79
Andrii <andrejsalogub3@gmail.com>
2024-03-05 14:02:21 +0200

    Added responsive markup and styles for the header section and made it responsive.

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import "../styles/Header.scss";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="header__title">
+        <a href="">CV Generator</a>
+      </h1>
+      <nav className="header__navigation">
+        <ul className="header__navigation-menu">
+          <li className="header__navigation-item">
+            <a href="">Edit CV</a>
+          </li>
+          <li className="header__navigation-item">
+            <a href="">Preview CV</a>
+          </li>
+          <li className="header__navigation-item">
+            <button className="header__upload-btn">Fill Example CV</button>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
