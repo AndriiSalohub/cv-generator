e207fc031820cf42d692a55c93d17feb4840a095
Andrii <andrejsalogub3@gmail.com>
2024-03-06 12:59:29 +0200

    Added markup and styles for the profile section and made it responsive.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <>
       <Header />
-      <main className="main"></main>
+      <main className="main">
+        <Profile />
+      </main>
       <Footer />
     </>
   );
---
+++ src/components/Profile.jsx
@@ -0,0 +1,38 @@
+import "../styles/Profile.scss";
+
+const Profile = () => {
+  return (
+    <section className="profile">
+      <div className="profile__image-placeholder"></div>
+      <div className="profile__image-info">
+        <h2 className="profile__image-title">Profile Picture</h2>
+        <p className="profile__image-description">
+          Adding a photo to your CV can be a great way to personalize your
+          application and make a positive first impression. When uploading your
+          photo, make sure it is a clear and recent headshot that shows your
+          face in a professional manner.
+        </p>
+        <p className="profile__image-description">
+          When taking your photo, dress professionally and{" "}
+          <span>consider the background and lighting.</span> A neutral
+          background and good lighting can help to highlight your features and
+          present you in the best possible light. Remember, your photo is an
+          important part of your CV, so take the time to choose a high-quality
+          image that represents you in a professional and approachable manner.
+        </p>
+        <p className="profile__image-description">
+          When you're done, the next step is to{" "}
+          <span>
+            start filling your application with your personal information,
+          </span>{" "}
+          good luck!
+        </p>
+        <div className="profile__image-choice">
+          <button className="profile__image-choose-btn">Choose file</button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Profile;
