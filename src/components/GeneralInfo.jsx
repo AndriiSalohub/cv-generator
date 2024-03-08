5448ebf8cb6a08f5bdd9dd888da1f8e064c20c65
Andrii <andrejsalogub3@gmail.com>
2024-03-08 14:30:03 +0200

    Added markup and styles for the general information form section.

--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -15,7 +15,114 @@ const GeneralInfo = () => {
           career goals.
         </p>
       </header>
-      <fomr className="general-info__form"></fomr>
+      <fomr className="general-info__form">
+        <div className="general-info__full-name">
+          <div>
+            {" "}
+            <label htmlFor="first name" className="general-info__form-label">
+              First name
+            </label>
+            <input
+              type="text"
+              name="first name"
+              id="first name"
+              className="general-info__form-input"
+            />
+          </div>
+          <div>
+            {" "}
+            <label htmlFor="last name" className="general-info__form-label">
+              Last name
+            </label>
+            <input
+              type="text"
+              name="last name"
+              id="last name"
+              className="general-info__form-input"
+            />
+          </div>
+        </div>
+        <label htmlFor="profession" className="general-info__form-label">
+          Profession
+        </label>
+        <input
+          type="text"
+          name="profession"
+          id="profession"
+          placeholder="e.g. Web Developer"
+          className="general-info__form-input"
+        />
+        <label htmlFor="location" className="general-info__form-label">
+          City and Country:
+        </label>
+        <input
+          type="text"
+          name="location"
+          id="location"
+          placeholder="e.g. Atlanta Developer"
+          className="general-info__form-input"
+        />
+        <div className="general-info__contacts">
+          <div>
+            <label htmlFor="linkedIn" className="general-info__form-label">
+              LinkedIn username
+            </label>
+            <input
+              type="text"
+              name="linkedIn"
+              id="linkedIn"
+              placeholder="e.g. andriisalohub"
+              className="general-info__form-input"
+            />
+          </div>
+          <div>
+            <label htmlFor="portfolio" className="general-info__form-label">
+              Portfolio URL
+            </label>
+            <input
+              type="url"
+              name="portfolio"
+              id="portfolio"
+              placeholder="e.g. https://github.com/AndriiSalohub/"
+              className="general-info__form-input"
+            />
+          </div>
+          <div>
+            <label htmlFor="email" className="general-info__form-label">
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="e.g. example@gmail.com"
+              className="general-info__form-input"
+            />
+          </div>
+          <div>
+            <label htmlFor="phone number" className="general-info__form-label">
+              Phone number
+            </label>
+            <input
+              type="tel"
+              name="phone number"
+              id="phone number"
+              placeholder="e.g. +38 067452 1334"
+              className="general-info__form-input"
+            />
+          </div>
+        </div>
+        <label htmlFor="desctiption" className="general-info__form-label">
+          Desctiption(About me)
+        </label>
+        <textarea
+          name="desctiption"
+          id="desctiption"
+          rows="6"
+          placeholder="Tell us about yourself! This is your chance to showcase your skills, experience, and personality. Write a brief summary of who you are and what you can bring to the table."
+          className="general-info__form-input"
+        ></textarea>
+      </fomr>
     </section>
   );
 };
