7a3554ada6b0e93f3afdb8372bf4ebf90727f4d7
Andrii <andrejsalogub3@gmail.com>
2024-03-20 12:04:07 +0200

    Added functionality to add education button.

--- src/components/EducationInfo/EducationInfo.jsx
+++ src/components/EducationInfo/EducationInfo.jsx
@@ -1,78 +1,125 @@
+import { useState } from "react";
 import FormDescription from "../FormDescription/FormDescription.jsx";
 import "./EducationInfo.scss";
+import { RiDeleteBin5Line } from "react-icons/ri";
 
 const EducationInfo = () => {
-    return (
-        <section className="education">
-            <FormDescription
-                titleNumber="2"
-                title="Education"
-                description="The education demonstrates your commitment to learning and your willingness to acquire new skills. If you have multiple degrees, list them in reverse chronological order, with the most recent degree listed first and if you don't have any formal education, consider including any relevant certifications or training you may have received."
-                addMoreButton={true}
-                buttonText="Add more education"
+  const [educations, setEducation] = useState([
+    {
+      id: 1,
+      schoolName: "",
+      degree: "",
+      startingDate: "",
+      endingDate: "",
+    },
+  ]);
+
+  console.log(educations.length);
+
+  const handleAddEducation = () => {
+    setEducation([
+      ...educations,
+      {
+        id: educations[educations.length - 1].id + 1,
+        schoolName: "",
+        degree: "",
+        startingDate: "",
+        endingDate: "",
+      },
+    ]);
+  };
+
+  const handleDeleteExperience = (event, id) => {
+    event.preventDefault();
+    if (educations.length > 1) {
+      const updatedExperiences = [...educations];
+      updatedExperiences.splice(id - 1, 1);
+      setEducation(updatedExperiences);
+    }
+  };
+
+  return (
+    <section className="education">
+      <FormDescription
+        titleNumber="2"
+        title="Education"
+        description="The education demonstrates your commitment to learning and your willingness to acquire new skills. If you have multiple degrees, list them in reverse chronological order, with the most recent degree listed first and if you don't have any formal education, consider including any relevant certifications or training you may have received."
+        addMoreButton={true}
+        buttonText="Add more education"
+        handleClick={handleAddEducation}
+      />
+      {educations.map((education, id) => (
+        <form action="" className="education__form form" key={id}>
+          <div>
+            <label
+              htmlFor="school name"
+              className="education__form-label form-label"
+            >
+              School name
+            </label>
+            <input
+              type="text"
+              name="school name"
+              id="school name"
+              className="education__form-input form-input"
             />
-            <form action="" className="education__form form">
-                <div>
-                    <label
-                        htmlFor="school name"
-                        className="education__form-label form-label"
-                    >
-                        School name
-                    </label>
-                    <input
-                        type="text"
-                        name="school name"
-                        id="school name"
-                        className="education__form-input form-input"
-                    />
-                </div>
-                <div>
-                    <label
-                        htmlFor="degree"
-                        className="education__form-label form-label"
-                    >
-                        Degree, certification or title
-                    </label>
-                    <input
-                        type="text"
-                        name="degree"
-                        id="degree"
-                        className="education__form-input form-input"
-                    />
-                </div>
-                <div>
-                    <label
-                        htmlFor="starting date"
-                        className="education__form-label form-label"
-                    >
-                        Starting date
-                    </label>
-                    <input
-                        type="text"
-                        name="starting date"
-                        id="starting date"
-                        className="education__form-input form-input"
-                        placeholder="MM/YYYY"
-                    />
-                </div>
-                <div>
-                    <label
-                        htmlFor="ending date"
-                        className="education__form-label form-label"
-                    >
-                        Ending date
-                    </label>
-                    <input
-                        type="text"
-                        name="ending date"
-                        id="ending date"
-                        className="education__form-input form-input"
-                        placeholder="MM/YYYY"
-                    />
-                </div>
-            </form>
-        </section>
-    );
+          </div>
+          <div>
+            <label
+              htmlFor="degree"
+              className="education__form-label form-label"
+            >
+              Degree, certification or title
+            </label>
+            <input
+              type="text"
+              name="degree"
+              id="degree"
+              className="education__form-input form-input"
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="starting date"
+              className="education__form-label form-label"
+            >
+              Starting date
+            </label>
+            <input
+              type="text"
+              name="starting date"
+              id="starting date"
+              className="education__form-input form-input"
+              placeholder="MM/YYYY"
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="ending date"
+              className="education__form-label form-label"
+            >
+              Ending date
+            </label>
+            <input
+              type="text"
+              name="ending date"
+              id="ending date"
+              className="education__form-input form-input"
+              placeholder="MM/YYYY"
+            />
+          </div>
+          {educations.length > 1 && (
+            <button
+              className="education__form-delete-btn delete-btn"
+              onClick={(event) => handleDeleteExperience(event, id)}
+            >
+              <RiDeleteBin5Line />
+            </button>
+          )}
+        </form>
+      ))}
+    </section>
+  );
 };
 
 export default EducationInfo;
